9b9621871db55e99a4f3cad5e944ca33641bc6eb
Vitalii Aleksandrov <vitaliy0508@ukr.net>
2022-12-05 19:30:20 +0200

    add redux-persist issue fix

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -3,12 +3,14 @@ import { nanoid } from 'nanoid';
 import { persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
-const contactsInitialState = [
-  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-];
+const contactsInitialState = {
+  contactsList: [
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ],
+};
 
 const contactsSlice = createSlice({
   name: 'contacts',
@@ -16,7 +18,7 @@ const contactsSlice = createSlice({
   reducers: {
     addContact: {
       reducer(state, action) {
-        state.push(action.payload);
+        state.contactsList.push(action.payload);
       },
       prepare(name, number) {
         return {
@@ -29,8 +31,10 @@ const contactsSlice = createSlice({
       },
     },
     deleteContact(state, action) {
-      const index = state.findIndex(contact => contact.id === action.payload);
-      state.splice(index, 1);
+      const index = state.contactsList.findIndex(
+        contact => contact.id === action.payload
+      );
+      state.contactsList.splice(index, 1);
     },
   },
 });
